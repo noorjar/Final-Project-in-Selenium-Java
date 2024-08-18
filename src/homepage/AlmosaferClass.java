254da7f1bc702983b20b83226041614c258bd039
noorjar <jaradatnooralhuda@gmail.com>
2024-08-19 00:32:11 +0300

    update

--- src/homepage/AlmosaferClass.java
+++ src/homepage/AlmosaferClass.java
@@ -1,5 +1,7 @@
 package homepage;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
@@ -12,39 +14,39 @@ public class AlmosaferClass {
 	public void mysetup() {
 		driver.manage().window().maximize();
 		driver.get(WebSiteLink);
-		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 	}
 	
 	@Test(priority = 1,enabled = true )
-	public void InitialLoadTest() { 
-		
-	}
+	public void DefaultLanguageTest() {}
+	@Test(priority = 2,enabled = true )
+	public void DefaultCurrencyTest() {}
+	@Test(priority = 3,enabled = true )
+	public void ContactInformationVerification() {}
+	@Test(priority = 4,enabled = true )
+	public void LogoVerification() {}
+	@Test(priority = 5,enabled = true )
+	public void HotelSearchTabDefaultState() {}
+	@Test(priority = 6,enabled = true )
+	public void FlightDepartureDate () {}
+	@Test(priority = 6,enabled = true )
+	public void FlightReturnDate    () {}
+	@Test(priority = 7,enabled = true )
+	public void InitialLoadTest() {}
 	
-	@Test(priority = 2,enabled = true)
-	public void LanguageSwitchTest() { 
-		
-	}
+	@Test(priority = 8,enabled = true)
+	public void LanguageSwitchTest() {}
 	
-	@Test(priority = 3,enabled = true)
-	public void HotelSearchTest() { 
-		
-	}
-	@Test(priority = 4,enabled = true)
-	public void RoomSelectionTes() { 
-		
-	}
+	@Test(priority = 9,enabled = true)
+	public void HotelSearchTest() {}
+	@Test(priority = 10,enabled = true)
+	public void RoomSelectionTes() {}
 	
-	@Test(priority = 5,enabled = true)
-	public void SearchButtonFunctionality() { 
-		
-	}		
-	@Test(priority = 6,enabled = true)
-	public void SearchResultsPageLoad() { 
-		
-	}
-	@Test(priority = 7,enabled = true)
-	public void PriceSortingTest() { 
-		
-	}
+	@Test(priority = 11,enabled = true)
+	public void SearchButtonFunctionality() {}		
+	@Test(priority = 12,enabled = true)
+	public void SearchResultsPageLoad() {}
+	@Test(priority = 13,enabled = true)
+	public void PriceSortingTest() {}
 
 }
